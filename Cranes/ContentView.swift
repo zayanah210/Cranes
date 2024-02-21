e1afc8ed7dc5ef9f28cf8f06d44df5572f5c3d33
Tuhin Mahmud <tuhinm@us.ibm.com>
2024-02-20 20:23:15 -0600

    Initial Commit

---
+++ Cranes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cranes
+//
+//  Created by Tuhin Mahmud on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cranes/CranesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CranesApp.swift
+//  Cranes
+//
+//  Created by Tuhin Mahmud on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CranesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
